654f8a06ee70acc4c3b467af5d151025634bd65e
anand008186 <anand_r@mt.iitr.ac.in>
2025-06-11 12:45:19 +0530

    updated to one column

--- src/components/PlanJourney.tsx
+++ src/components/PlanJourney.tsx
@@ -564,8 +564,8 @@ const existingMarkerRef = useRef<mapboxgl.Marker | null>(null); // Ref to store 
             </p>
           </div> */}
         </div>
-        <div className="grid grid-cols-1 min-[575px]:grid-cols-2 gap-6 px-4">
-          <div className="relative w-full" style={{ aspectRatio: '1/1' }}>
+        <div className="grid grid-cols-1 gap-6 px-4">
+          <div className="relative w-full h-[500px] " style={{ aspectRatio: '1/1' }}>
             <div
               ref={mapContainerRef}
               className="absolute inset-0 w-full h-full rounded-lg overflow-hidden border border-gray-200"
